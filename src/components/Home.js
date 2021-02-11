41cc992666b207c57bbc2cb2ca1dfafa0940de4b
Angelica <angelikcu4@gmail.com>
2021-02-11 16:17:50 -0500

    ajustes vista home

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,8 +1,9 @@
 
 import React, { useState } from 'react';
-import { StyleSheet, Text, View, TouchableOpacity, Image, ScrollView } from 'react-native';
+import { StyleSheet, Text, View, TouchableOpacity, Image, ScrollView, Modal } from 'react-native';
 import { Picker, Container, Content, CardItem, Left, Body, Button, Card, Title } from "native-base";
 const header = require('../../assets/Header.png');
+
 
 import Calendar from '../components/calendarHome'
 
@@ -10,8 +11,7 @@ import Calendar from '../components/calendarHome'
 
 import data from '../../data/events.json'
 
-console.log(data.events);
-const dataEvents = data.events;
+let dataEvents = data.events;
 
 const event = {
   name: "Recorrido bicicleta",
@@ -35,96 +35,99 @@ const handlePressButton = () => {
 const CardEvent = ({ objNavigate }) => {
   
   return (
-    <ScrollView contentContainerStyle={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Content padder>{
+      <Content style={{ flex: 1 , flexDirection: 'row' }} padder>{
         dataEvents.map((events, index) => {
           return (
-            <ScrollView>
-              <Card style={{ flex: 1, flexDirection: 'row' }}>
+              <Card style={{ flex: 2, flexDirection: 'row', }} >
                 <CardItem>
                   <Left>
                     <Body>
-                      <Image source={{ uri: 'https://upload.wikimedia.org/wikipedia/commons/thumb/7/7d/Monserrate_Sanctuary.JPG/1200px-Monserrate_Sanctuary.JPG' }} style={{ height: 100, width: 100, flex: 1, borderRadius: 5 }} />
+                      <Image source={{ uri: 'https://upload.wikimedia.org/wikipedia/commons/thumb/7/7d/Monserrate_Sanctuary.JPG/1200px-Monserrate_Sanctuary.JPG' }} style={{ height: 80, width: 80, borderRadius: 5, margin: 0 }} />
                     </Body>
                   </Left>
                 </CardItem>
-
                 <CardItem style={{ flex: 2, flexDirection: 'column' }}>
-                  <Body>
-                    <Text style={{ color: '#584799', fontWeight: 600, fontSize: 20 }}>{events.name}</Text>
-                    <Text style={{ textAlign: 'justify', fontSize: 15 }}>{events.date}</Text>
+                  <Body style={{width:170, height:30}}>
+                    <Text style={ stylesHome.textName}>{events.name}</Text>
+                    <Text style={stylesHome.textDetails}>{events.details}</Text>
+                    <Text style={stylesHome.textDate}>{events.date}</Text>
                     <Button onPress={handlePressButton} title="crear recordatorio"></Button>
                   </Body>
-
                   <Body style={{ flex: 2, flexDirection: 'row' }}>
                     <TouchableOpacity style={stylesHome.boton} onPress={() => objNavigate.navigate('Evento')}>
-                      <Text style={{ textAlign: 'center' }} >Ver más</Text></TouchableOpacity>
+                      <Text style={stylesHome.textBoton} >Ver más</Text></TouchableOpacity>
                     <TouchableOpacity style={stylesHome.boton} onPress={() => objNavigate.navigate('Mi agenda')}>
-                <Text style={{ textAlign: 'center' }}>Reservar</Text>
+                <Text style={stylesHome.textBoton}>Reservar</Text>
               </TouchableOpacity>
                   </Body>
-
                 </CardItem>
               </Card>
-
-              
-            </ScrollView>
           )
         }
-
-          // <Text>{events.name}</Text>
-          // <EventHome
-          //   key={'e' + index}
-          //   events={events}/>
-          // console.log('hola');
-
         )
       }
-
       </Content>
-
-    </ScrollView>
   )
 }
 
 
 export default function Home({navigation}) {
   const [selectedValue, setSelectedValue] = useState("kennedy");
+  let mapCategory = dataEvents.map((element) => element.category);
+  let unicoCategory = [...new Set(mapCategory)];
+  console.log(unicoCategory);
+  let mapUbicacion = dataEvents.map((element, index)=> element.locality);
+  let unicaUbicacion = [...new Set(mapUbicacion)];
+
+
+  const localidades = ()=>{
+    let arrayLocality = [];
+    console.log(arrayLocality); 
+    for ( let i= 0; i < dataEvents.length; i++) {
+    let localidad = dataEvents[i].locality;
+    for (let j= 0; j < localidad.length; j++){
+      let allLocality = localidad[j];
+      arrayLocality.push(allLocality);
+    }
+    }
+  }
+  localidades();
+
     return (
-        <ScrollView>
+      <Container style={{ flex: 1 , justifyContent:'center', alignItems: 'center' }} >
+<Content>
+
         <Image  source={header} style={stylesHome.imagen} />
       <View style={stylesHome.containerSelect} >
       <Picker
         selectedValue={selectedValue}
         style={stylesHome.select} 
         onValueChange={(itemValue, itemIndex) => setSelectedValue(itemValue)}>
-        <Picker.Item label="Ubicación" value="java" />
-        <Picker.Item label="Kennedy" value="js" />
+        <Picker.Item label="Ubicación" value="java" padder/>{
+          unicaUbicacion.map((element)=>
+          <Picker.Item label={element} value="js" /> 
+          ) 
+        } 
       </Picker>
       <Picker
         selectedValue={selectedValue}
         style= {stylesHome.select} 
         onValueChange={(itemValue, itemIndex) => setSelectedValue(itemValue)} >
-        <Picker.Item label="Categorias" value="java" />
-        <Picker.Item label="Caminatas" value="js" />
+        <Picker.Item label="Categorias" value="java" padder/>{
+          unicoCategory.map((event) => 
+          <Picker.Item label={event} value="js"/>
+            )}
       </Picker>
       </View>
       <View>
-          <Content padder>{
-        dataEvents.map((events, index) => {
-            <CardEvent objNavigate={navigation}
-            events={events}/>            
-        }
-
-        )
-      }
-      </Content>
       <Calendar>        
       </Calendar>
-      <CardEvent />
-      <CardEvent />      
       </View>
-    </ScrollView>
+      <CardEvent objNavigate={navigation}/>            
+      </Content>
+      </Container>
+  
+        
     ) }
 
 const stylesHome = StyleSheet.create({
@@ -139,7 +142,9 @@ const stylesHome = StyleSheet.create({
     width: 80,
     height: 30,
     margin: 10,
-    borderRadius: 5
+    borderRadius: 5,
+    textAlign: 'center',
+    padding: 2,
   },
   imagen: {
     width: 300,
@@ -150,5 +155,23 @@ const stylesHome = StyleSheet.create({
     flex: 1,
     flexDirection: 'row',
     marginTop: 20
+  },
+  textBoton: {
+    color: '#584799',
+    fontSize: 17,
+    fontWeight: 600
+  },
+  textName:{
+    color: '#584799', 
+    fontWeight: 600, 
+    fontSize: 20
+  },
+  textDate :{ 
+    textAlign: 'justify',
+    fontSize: 15 },
+  textDetails: {
+    width: 190,
+    textAlign: 'justify',
+    marginBottom: 5
   }
 });
